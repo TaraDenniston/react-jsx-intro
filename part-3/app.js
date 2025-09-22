fe586b0ce1a701b94746a4a6ff0f866910479bde
TaraDenniston <taradenniston@gmail.com>
2025-09-22 14:28:32 -0400

    complete person component and add person info

--- part-3/app.js
+++ part-3/app.js
@@ -1,7 +1,21 @@
 const App = () => {
     return (
         <div>
-            <Person />
+            <Person 
+                name="Caleb" 
+                age={23} 
+                hobbies={['snow skiing', 'water skiing', 'camping', 'learning']}
+            />
+            <Person 
+                name="Alexandra" 
+                age={35} 
+                hobbies={['reading', 'yoga', 'laughing', 'baking']}
+            />
+            <Person 
+                name="August" 
+                age={16} 
+                hobbies={['playing games', 'drawing', 'crocheting']}
+            />
         </div>
     );
 };
--- part-3/person.js
+++ part-3/person.js
@@ -1,7 +1,19 @@
 const Person = (props) => {
+    let displayName = props.name.length > 8 ? props.name.slice(0, 6) : props.name;
+    let hobbyList = props.hobbies.map((hobby, index) => <li key={index}>{hobby}</li>);
     return (
         <div>
             <p>Learn some information about this person</p>
+            <p>
+                Name: {displayName} <br />
+                Age: {props.age} <br />
+                Hobbies:
+                <ul>
+                    {hobbyList}
+                </ul>
+            </p>
+            <h3>{props.name}, {props.age >= 18 ? "please go vote!" : "you must be 18 to vote"}</h3>
+            <hr />
         </div>
     );
 };
